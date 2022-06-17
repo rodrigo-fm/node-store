85691eecbe258f2595eef154732fafc937cbd61a
rodrigo-fm <rodrigo.martins@dcomp.ufs.br>
2022-06-16 21:37:12 -0300

    add better error message on try catch helper

--- src/project/1-presentation/helpers/TryCatchHelper.ts
+++ src/project/1-presentation/helpers/TryCatchHelper.ts
@@ -4,6 +4,12 @@ export default async function(callable: () => Promise<any>): Promise<any> {
     try {
         return await callable();
     } catch(error) {
-        return Promise.resolve(http500ServerError(error.message));
+        if(error.message !== undefined) {
+            return Promise.resolve(http500ServerError(error.message));
+        }
+        else if(error.constructor !== undefined && error.constructor.name !== undefined) {
+            return Promise.resolve(http500ServerError('Unknown server error: ' + error.constructor.name));
+        }
+        return Promise.resolve(http500ServerError('Unknown server error'));
     }
 }
