a9aa6cf19c4038f3acbd249ef336f3973278700c
rodrigo-fm <rodrigo.martins@dcomp.ufs.br>
2022-07-11 16:36:40 -0300

    create initial configuration of docker-compose.yaml

---
+++ src/project/main/config/connection/connection-development.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+
+export const devConnection = async (): Promise<DataSource> => {
+    try {
+        const datasource =  new DataSource({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root_password_docker',
+            database: 'node_store_db',
+            entities: [__dirname + '/4-infra/models/*.{js,ts}']
+        })
+    
+        await datasource.initialize();
+        console.log('development database connection initalized');
+        return datasource;
+    } catch(error) {
+        console.log('error connection to database: ' + JSON.stringify(error));
+    }
+};
